1390a7f735aa324399360af8b9732bc1d9916e2b
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2021-08-15 13:01:50 -0400

    better types

--- src/index.ts
+++ src/index.ts
@@ -7,49 +7,61 @@ type PrimitiveArray = Array<BasePrimitive | PrimitiveObject | PrimitiveArray>
 
 type AnyData = PrimitiveObject | PrimitiveArray | BasePrimitive
 
-type AsImmutable<Schema extends AnyData> = Schema extends BasePrimitive
-    ? Schema
-    : {
-          [Props in keyof Schema]-?: Schema[Props] extends
-              | PrimitiveObject
-              | PrimitiveArray
-              ? Immutable<Schema[Props]>
-              : Schema[Props]
-      }
+type Is<T extends U, U> = T
 
-type Paths<Schema extends AnyData> = Schema extends BasePrimitive
-    ? never
-    : {
-          [K in keyof Schema]-?: Schema[K] extends BasePrimitive
-              ? [K]
-              : Schema[K] extends Immutable<infer R>
-              ? [K, ...Paths<R>]
-              : Schema[K] extends PrimitiveObject | PrimitiveArray
-              ? [K, ...Paths<Schema[K]>]
-              : [K]
-      }[keyof Schema]
+type FilterKeys<T> = T extends readonly unknown[]
+    ? number extends T['length']
+        ? number
+        : `${number}`
+    : string | number
 
-type GetInSchema<
-    Schema extends PrimitiveObject | PrimitiveArray,
-    Keys = Paths<AsImmutable<Schema>>,
-> = Keys extends [infer E1]
-    ? E1 extends keyof Schema
-        ? Schema[E1]
-        : never
-    : Keys extends [infer E1, ...any[]]
-    ? E1 extends keyof Schema
+type AsImmutable<Schema> = Schema extends AnyData
+    ? Schema extends BasePrimitive
+        ? Schema
+        : Schema extends Is<
+              infer S,
+              { [P in keyof Schema & FilterKeys<Schema>]-?: Schema[P] }
+          >
         ? {
-              [U in E1]-?: Schema[U] extends PrimitiveObject | PrimitiveArray
-                  ? GetInSchema<Schema[U]>
-                  : never
-          }[E1]
+              [P in keyof S]-?: S[P] extends PrimitiveArray | PrimitiveObject
+                  ? Immutable<S[P]>
+                  : Readonly<S[P]>
+          }
         : never
     : never
 
-// type A = GetInSchema<
-//     { a: { c: { d: number; k: { r: 'hi' } } }; b: number },
-//     ['a', 'c', 'k', 'r']
-// >
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Paths<T> = T extends object
+    ? {
+          [K in keyof T & FilterKeys<T>]: [`${K}`] | [`${K}`, ...Paths<T[K]>]
+      }[keyof T & FilterKeys<T>]
+    : never
+
+// TypeScript does not like "0" extends keyof { 0: ... }, so we have to write out own descend
+type Descend<T, K extends string> = T extends { [P in K]?: infer X }
+    ? X | (T extends { [P in K]: any } ? never : undefined)
+    : T[K & keyof T] | undefined // index access type of T is to support indexed types
+
+type PickSplitPath<Schema, Keys extends string[]> = Keys extends [
+    Is<infer First, string>,
+    ...Is<infer Rest, string[]>
+]
+    ? Schema extends Schema
+        ? { [P in First]: PickSplitPath<Descend<Schema, First>, Rest> }[First]
+        : never
+    : Schema
+
+type GetInSchema<
+    Schema extends PrimitiveObject | PrimitiveArray,
+    Keys extends Paths<Schema>,
+> = PickSplitPath<Schema, Keys>
+
+type S = {
+    abc: string
+    nested: { key: string; someArray: (string | number)[] }
+    really_nested: { level1: { level2: string } }
+}
+type B = GetInSchema<S, Paths<S>> // why is B unknown. Must be some intersection of Paths? need to filter it?
 
 function isBasePrimitive(value: unknown): value is BasePrimitive {
     return (
@@ -59,6 +71,10 @@ function isBasePrimitive(value: unknown): value is BasePrimitive {
         typeof value === 'boolean' ||
         typeof value === 'number'
     )
+}
+
+function hasKey<T>(obj: T, key: any): key is keyof T {
+    return key in obj
 }
 class Immutable<Schema extends PrimitiveObject | PrimitiveArray> {
     private value: AsImmutable<Schema>
@@ -82,12 +98,18 @@ class Immutable<Schema extends PrimitiveObject | PrimitiveArray> {
         return this.value[key]
     }
 
-    getIn(keys: Paths<AsImmutable<Schema>>): GetInSchema<Schema> {
-        // if (!Array.isArray(keys)) throw new Error('Keys must be array.')
-        if (keys.length === 1) {
-            return this.get(keys[0])
+    getIn<R = AsImmutable<GetInSchema<Schema, Paths<Schema>>>>([
+        zeroth,
+        ...rest
+    ]: Paths<Schema>): R | undefined {
+        if (hasKey(this.value, zeroth)) {
+            const el = this.value[zeroth]
+            if (rest.length && el instanceof Immutable) {
+                return el.getIn(rest as any) as R
+            }
+            return el as unknown as R
         }
-        return this.get(keys[0]).getIn(keys.slice(1))
+        return undefined
     }
 }
 
--- tests/immutable.test.ts
+++ tests/immutable.test.ts
@@ -26,6 +26,8 @@ describe('getIn', () => {
             really_nested: { level1: { level2: 'value!' } },
         })
 
+        const a = obj.getIn(['abc'])
+
         expect(obj.getIn(['abc'])).toEqual('some value')
         expect(obj.getIn(['nested', 'key'])).toEqual('hi')
         expect(obj.getIn(['nested', 'someArray', 1])).toEqual(2)
